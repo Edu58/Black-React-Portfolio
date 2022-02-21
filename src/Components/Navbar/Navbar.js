886d56ec4dc92f2673f297632b73bf9f7a7fe55c
Edu58 <edumuriithi58@gmail.com>
2022-02-21 16:09:32 +0300

    mobile responsive

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,19 +1,20 @@
 import style from './Navbar.module.css';
+import { BsCardText } from "react-icons/bs";
 
 function Navbar() {
     return ( 
         <nav>
-        <div className={style.logo}>
-            <h1>DevEdwin</h1>
-        </div>
-        
-        <div className={style.navlinks}>
-            <ul>
-              <li>Work</li>
-              <li>Capabilities</li>
-              <li>Contact</li>
-            </ul>
-        </div>
+          <div className={style.logo}>
+              <h1>DevEdwin</h1>
+          </div>
+          
+          <div className={style.navlinks}>
+              <ul className={style.navLi} id="navlinks">
+                <li className={style.work}>Work</li>
+                <li className={style.capabilities}>Capabilities</li>
+                <li>Contact</li>
+              </ul>
+          </div>
       </nav>
      );
 }
